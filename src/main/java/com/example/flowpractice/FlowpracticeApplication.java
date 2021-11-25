e66dc9e5858d7d8f75c87c060097ca75665807ef
Tomas Padilla <padillatomasagustin@gmail.com>
2021-11-25 11:34:34 +0100

    Running SB App

---
+++ src/main/java/com/example/flowpractice/FlowpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowpracticeApplication.class, args);
+	}
+
+}
